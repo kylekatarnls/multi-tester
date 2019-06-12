8a4110214b44d08fe68952157e5d1e390b3daa5b
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-06-12 15:42:01 +0300

    mod

--- src/MultiTester/Summary.php
+++ src/MultiTester/Summary.php
@@ -43,30 +43,33 @@ class Summary
     public function get()
     {
         $count = count($this->state);
-        $pad = max(array_map('strlen', array_keys($this->state)));
-        $successString = '%s    Success';
-        $failureString = '%s    > Failure!';
-        $successFinalString = '%d / %d     No project broken by current changes.';
-        $failureFinalString = '%d / %d     %s broken by current changes.';
-        $passed = 0;
+        if ($count > 0) {
+            $pad = max(array_map('strlen', array_keys($this->state)));
+            $successString = '%s    Success';
+            $failureString = '%s    > Failure!';
+            $successFinalString = '%d / %d     No project broken by current changes.';
+            $failureFinalString = '%d / %d     %s broken by current changes.';
+            $passed = 0;
 
-        if ($this->isColored()) {
-            $successString = "\033[42;97m $successString \033[0m";
-            $failureString = "\033[41;97m %s    Failure \033[0m";
-            $successFinalString = "\033[42;97m %d / %d     No project broken by current changes. \033[0m";
-            $failureFinalString = "\033[41;97m %d / %d     %s broken by current changes. \033[0m";
+            if ($this->isColored()) {
+                $successString = "\033[42;97m $successString \033[0m";
+                $failureString = "\033[41;97m %s    Failure \033[0m";
+                $successFinalString = "\033[42;97m %d / %d     No project broken by current changes. \033[0m";
+                $failureFinalString = "\033[41;97m %d / %d     %s broken by current changes. \033[0m";
+            }
+
+            $output = '';
+
+            foreach ($this->state as $package => $success) {
+                $passed += $success ? 1 : 0;
+                $output .= sprintf($success ? $successString : $failureString, str_pad($package, $pad)) . "\n";
+            }
+
+            $output .= "\n" . sprintf($passed === $count ? $successFinalString : $failureFinalString, $passed, $count,
+                    ($count - $passed) . ' project' . ($count - $passed > 1 ? 's' : '')) . "\n";
+        } else {
+            $output = 'No project(s) tested, empty config file?' . PHP_EOL;
         }
-
-        $output = '';
-
-        foreach ($this->state as $package => $success) {
-            $passed += $success ? 1 : 0;
-            $output .= sprintf($success ? $successString : $failureString, str_pad($package, $pad)) . "\n";
-        }
-
-        $output .= "\n" . sprintf($passed === $count ? $successFinalString : $failureFinalString, $passed, $count,
-                ($count - $passed) . ' project' . ($count - $passed > 1 ? 's' : '')) . "\n";
-
         return $output;
     }
 
